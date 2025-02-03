ca176b19a128a3056d04b4f97f9e4da4d3e62879
Niriel <bdelforge@protonmail.com>
2025-02-03 15:13:01 +0100

    Move list controller out into its directory

--- php-boodschappenopdracht/index.php
+++ php-boodschappenopdracht/index.php
@@ -1,15 +1,4 @@
 <?php
 
-$groceries = [
-    ['name' => 'Brood', "price" => 1.00, 'quantity' => 0],
-    ['name' => 'Broccoli', "price" => 0.99, 'quantity' => 1],
-    ['name' => 'Krentenbollen', "price" => 1.20, 'quantity' => 2],
-    ['name' => 'Noten', "price" => 2.99, 'quantity' => 3],
-];
-
-$total_cost = array_reduce($groceries, function($acc, $item) {
-    return $acc + $item['price'] * $item['quantity'];
-}, 0);
-
-require("views/index.view.php");
+require("controllers/list.php");
 ?>
