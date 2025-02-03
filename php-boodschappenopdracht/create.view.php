f9bf951174844dfcf790b34b536e8fd7ecccb715
Niriel <bdelforge@protonmail.com>
2025-02-03 13:56:33 +0100

    Close unclosed tags

--- php-boodschappenopdracht/create.view.php
+++ php-boodschappenopdracht/create.view.php
@@ -18,15 +18,15 @@
         <form>
             <ul>
                 <li>
-                    <label for="productName">Naam:
+                    <label for="productName">Naam:</label>
                     <input type="text" id="productName" name="productName" value="brood">
                 </li>
                 <li>
-                    <label for="productPrice">Prijs per stuk:
+                    <label for="productPrice">Prijs per stuk:</label>
                     <input type="number" id="productPrice" name="productPrice" min="0" step="0.01" value="1.00">
                 </li>
                 <li>
-                    <label for="productQuantity">Aantal:
+                    <label for="productQuantity">Aantal:</label>
                     <input type="number" id="productQuantity" name="productQuantity" min="0" step="1" value="1">
                 </li>
             </ul>
