c09d18d98692c4e5e06e277911802021ad73809d
Niriel <bdelforge@protonmail.com>
2025-02-05 10:30:36 +0100

    Prototype highlight current navbar item

--- php-boodschappenopdracht/views/partials/nav.php
+++ php-boodschappenopdracht/views/partials/nav.php
@@ -1,6 +1,20 @@
+<?php
+function urlIs($url) {
+    return $_SERVER['REQUEST_URI'] == $url;
+}
+
+function highlighIfUrlIs($url) {
+    if (urlIs($url)) {
+        return 'highlight';
+    } else {
+        return '';
+    }
+}
+?>
+
 <nav>
     <ul class="navItems">
-        <li class="navItem"><a href="/">Lijst</a></li>
-        <li class="navItem"><a href="/controllers/create.php">Toevoegen</a></li>
+        <li class="navItem <?= highlighIfUrlIs('/') ?>"><a href="/">Lijst</a></li>
+        <li class="navItem <?= highlighIfUrlIs('/controllers/create.php') ?>"><a href="/controllers/create.php">Toevoegen</a></li>
     </ul>
 </nav>
