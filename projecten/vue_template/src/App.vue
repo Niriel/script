3a6152387e5d9e73e4268b8c93b0237eb58ead63
Niriel <bdelforge@protonmail.com>
2025-02-17 14:02:46 +0100

    De v-for directive
     Hoofdstuk Vue, Opdracht 5

--- projecten/vue_template/src/App.vue
+++ projecten/vue_template/src/App.vue
@@ -18,17 +18,43 @@ function resetCountry() {
 
 resetFirstName();
 resetCountry();
+//====================================================
+const tasks = ref([
+    {name: 'Boodschappen doen', completed: false},
+    {name: 'Afwassen', completed: true},
+    {name: 'Hond uitlaten', completed: false},
+]);
+
+function toggleTaskCompletion(task) {
+    task.completed = !task.completed;
+}
 </script>
 
 <template>
-    <h1>{{ person.firstName }} from {{ person.country }}</h1>
-    <button type="button" @click="incClicks">Click {{ clicks }}</button>
-    <button type="button" @click="resetFirstName">Reset first name</button>
-    <button type="button" @click="resetCountry">Reset country</button>
-    <br />
-    <input type="text" v-model="person.firstName" />
-    from
-    <input type="text" v-model="person.country" />
+    <section>
+        <div>
+            <h2>Hello, {{ person.firstName }} from {{ person.country }}</h2>
+            <button type="button" @click="incClicks">Click {{ clicks }}</button>
+            <button type="button" @click="resetFirstName">Reset first name</button>
+            <button type="button" @click="resetCountry">Reset country</button>
+            <br />
+            <input type="text" v-model="person.firstName" />
+            from
+            <input type="text" v-model="person.country" />
+        </div>
+    </section>
+    <hr />
+    <section>
+        <div>
+            <h2>Things to do</h2>
+            <ul>
+                <li v-for="(task, index) in tasks" :key="index">
+                    {{ task.name }}, completed: {{ task.completed }}
+                    <button type="button" @click="toggleTaskCompletion(task)">toggle completed</button>
+                </li>
+            </ul>
+        </div>
+    </section>
 </template>
 
 <style scoped></style>
