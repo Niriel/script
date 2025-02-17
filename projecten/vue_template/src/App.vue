56d83ba991f160a84e32afc7443a87613336c3e9
Niriel <bdelforge@protonmail.com>
2025-02-17 13:42:02 +0100

    Ref & Reactive
     Hoofdstuk Vue, Opdracht 4

---
+++ projecten/vue_template/src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import {ref, reactive} from 'vue';
+
+const clicks = ref(0);
+const person = reactive({firstName: '', country: ''});
+
+function incClicks() {
+    clicks.value++;
+}
+
+function resetFirstName() {
+    person.firstName = 'Garry';
+}
+
+function resetCountry() {
+    person.country = 'Morrowind';
+}
+
+resetFirstName();
+resetCountry();
+</script>
+
+<template>
+    <h1>{{ person.firstName }} from {{ person.country }}</h1>
+    <button type="button" @click="incClicks">Click {{ clicks }}</button>
+    <button type="button" @click="resetFirstName">Reset first name</button>
+    <button type="button" @click="resetCountry">Reset country</button>
+    <br />
+    <input type="text" v-model="person.firstName" />
+    from
+    <input type="text" v-model="person.country" />
+</template>
+
+<style scoped></style>
