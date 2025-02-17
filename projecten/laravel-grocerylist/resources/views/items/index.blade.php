7a82d45a1eb48b347414bd83c36bd44925cb9d73
Niriel <bdelforge@protonmail.com>
2025-02-17 10:58:46 +0100

    Make the list of items less ugly

--- projecten/laravel-grocerylist/resources/views/items/index.blade.php
+++ projecten/laravel-grocerylist/resources/views/items/index.blade.php
@@ -3,35 +3,36 @@
 @section('title', 'Grocery list items')
 
 @section('content')
-    <h1>Items</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>Naam</th>
-                <th>Bescrijving</th>
-                <th>Acties</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($items as $item)
-            <tr>
-                <td>
-                    <a href="{{ route('items.show', $item->id) }}">
+<h1>Items</h1>
+<table>
+    <thead>
+        <tr>
+            <th>Naam</th>
+            <th>Bescrijving</th>
+            <th colspan="2">Acties</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($items as $item)
+        <tr>
+            <td>
+                <a href="{{ route('items.show', $item->id) }}">
                     {{ $item->name }}
-                    </a>
-                </td>
-                <td>{{ $item->description }}</td>
-                <td>
-                    <a href="{{ route('items.edit', $item->id) }}">Bewerken</a>
-                    /
-                    <form action="{{ route('items.destroy', $item->id) }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit">Verwijderen</button>
-                    </form>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+                </a>
+            </td>
+            <td>{{ $item->description }}</td>
+            <td>
+                <a href="{{ route('items.edit', $item->id) }}">Bewerken</a>
+            </td>
+            <td>
+                <form action="{{ route('items.destroy', $item->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit">Verwijderen</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
 @endsection
