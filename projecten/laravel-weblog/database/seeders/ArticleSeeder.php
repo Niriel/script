d921d2b78f19ba4b9e9e22fc04aeadccefae70b5
Niriel <bdelforge@protonmail.com>
2025-02-21 13:35:42 +0100

    Seed pivot table

--- projecten/laravel-weblog/database/seeders/ArticleSeeder.php
+++ projecten/laravel-weblog/database/seeders/ArticleSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Article;
+use App\Models\Category;
 
 class ArticleSeeder extends Seeder
 {
@@ -13,6 +14,14 @@ class ArticleSeeder extends Seeder
      */
     public function run(): void
     {
-        Article::factory()->count(20)->create();
+        $articles = Article::factory()->count(20)->create();
+
+        $categoryCount = Category::count();
+
+        $articles->each(function (Article $article) use ($categoryCount) {
+            for ($i = 0; $i < rand(1, $categoryCount); $i++) {
+                $article->categories()->syncWithoutDetaching(rand(1, $categoryCount));
+            }
+        });
     }
 }
