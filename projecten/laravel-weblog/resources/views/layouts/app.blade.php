28b686df660fa5b233b6c80192e08e18c125ed45
Niriel <bdelforge@protonmail.com>
2025-02-24 10:12:18 +0100

    Add stylesheet

--- projecten/laravel-weblog/resources/views/layouts/app.blade.php
+++ projecten/laravel-weblog/resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Scriptlog - @yield('title')</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('style.css') }}">
 </head>
 
 <body>
