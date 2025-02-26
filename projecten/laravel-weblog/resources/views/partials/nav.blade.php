4919b3c7b223ed10c8a71cc8e93421f6888d4831
Niriel <bdelforge@protonmail.com>
2025-02-26 11:34:25 +0100

    Fix navbar, take auth into account

--- projecten/laravel-weblog/resources/views/partials/nav.blade.php
+++ projecten/laravel-weblog/resources/views/partials/nav.blade.php
@@ -1,11 +1,15 @@
 <nav>
     <div id="navigation">
         <ul>
-            <li class="navitem"><a href="{{ route('articles.index') }}">Home</a>
-            <li class="navitem"><a href="{{ route('dashboard.index') }}">Dashboard</a>
-            <li class="navitem"><a href="{{ route('articles.create') }}">Create</a>
-            <li class="navitem"><a href="{{ route('login.index') }}">Log in</a>
-            <li class="navitem"><a href="{{ route('logout.perform') }}">Log out</a>
+            <li class="navitem"><a href="{{ route('articles.index') }}">Home</a></li>
+            <li class="navitem"><a href="{{ route('dashboard.index') }}">Dashboard</a></li>
+            <li class="navitem"><a href="{{ route('articles.create') }}">Create</a></li>
+            @guest
+            <li class="navitem"><a href="{{ route('login.index') }}">Log in</a></li>
+            @endguest
+            @auth
+            <li class="navitem"><a href="{{ route('logout.perform') }}">Log out</a></li>
+            @endauth
         </ul>
     </div>
     <hr />
