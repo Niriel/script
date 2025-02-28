a41675f73edbe26e2d687938a6287b92b4898efc
Niriel <bdelforge@protonmail.com>
2025-02-28 11:42:16 +0100

    Apply Auth middleware to ensure user is logged in

--- projecten/laravel-weblog/routes/web.php
+++ projecten/laravel-weblog/routes/web.php
@@ -5,24 +5,26 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\LoginController;
 use App\Http\Controllers\AuthController;
 
-Route::get('/articles',                [ArticleController::class, 'index'])->name('articles.index');
-Route::get('/articles/create',         [ArticleController::class, 'create'])->name('articles.create');
-Route::post('/articles',               [ArticleController::class, 'store'])->name('articles.store');
-Route::get('/articles/{article}',      [ArticleController::class, 'show'])->name('articles.show');
-Route::get('/articles/{article}/edit', [ArticleController::class, 'edit'])->name('articles.edit');
-Route::put('/articles/{article}',      [ArticleController::class, 'update'])->name('articles.update');
-Route::delete('/articles/{article}',   [ArticleController::class, 'destroy'])->name('articles.destroy');
+Route::get('/articles',           [ArticleController::class, 'index'    ])->name('articles.index');
+Route::get('/articles/{article}', [ArticleController::class, 'show'     ])->name('articles.show');
+Route::get('/login',              [AuthController::class,    'loginPage'])->name('auth.loginPage');
+Route::post('/login',             [AuthController::class, '   login'    ])->name('auth.login');
 
-Route::post('/comments',               [CommentController::class, 'store'])->name('comments.store');
-Route::delete('/comments/{comment}',   [CommentController::class, 'destroy'])->name('comments.destroy');
+Route::group(['middleware' => ['auth']], function() {
+    Route::get('/articles/create',         [ArticleController::class, 'create' ])->name('articles.create');
+    Route::post('/articles',               [ArticleController::class, 'store'  ])->name('articles.store');
+    Route::get('/articles/{article}/edit', [ArticleController::class, 'edit'   ])->name('articles.edit');
+    Route::put('/articles/{article}',      [ArticleController::class, 'update' ])->name('articles.update');
+    Route::delete('/articles/{article}',   [ArticleController::class, 'destroy'])->name('articles.destroy');
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+    Route::post('/comments',               [CommentController::class, 'store'  ])->name('comments.store');
+    Route::delete('/comments/{comment}',   [CommentController::class, 'destroy'])->name('comments.destroy');
 
-Route::get('/login', [AuthController::class, 'loginPage'])->name('auth.loginPage');
-Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
-Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+
+    Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+});
 
 Route::redirect('/', '/articles');
