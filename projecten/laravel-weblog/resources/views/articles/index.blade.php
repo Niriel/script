594ee678d01ec1878b7ae7920e4c36511a6d7995
Niriel <bdelforge@protonmail.com>
2025-02-28 15:16:11 +0100

    Clean articles index html

--- projecten/laravel-weblog/resources/views/articles/index.blade.php
+++ projecten/laravel-weblog/resources/views/articles/index.blade.php
@@ -3,27 +3,29 @@
 @section('title', 'Articles')
 
 @section('content')
-<ul>
-    @foreach($articles as $article)
-    <li>
-        <article>
-            <h2 class="article_title {{ $article->is_premium ? 'premium' : ''}}">
-                <a href="{{ route('articles.show', $article->id) }}">{{ $article->title }}</a>
-                {{ $article->is_premium ? 'Premium' : ''}}
-            </h2>
-            <div class="article_header">
-                {{ $article->created_at }}, by
-                {{ $article->user->name }}
+<section id="article_list">
+    <ul>
+        @foreach($articles as $article)
+        <li>
+            <article>
+                <h2 class="article_title {{ $article->is_premium ? 'premium' : ''}}">
+                    <a href="{{ route('articles.show', $article->id) }}">{{ $article->title }}</a>
+                    {{ $article->is_premium ? 'Premium' : ''}}
+                </h2>
+                <div class="article_header">
+                    {{ $article->created_at }}, by
+                    {{ $article->user->name }}
 
-            </div>
-            <blockquote class="article_excerpt">
-                {{ substr($article->content, 0, 500) }}
-                @if(mb_strlen($article->content) >= 500)
+                </div>
+                <blockquote class="article_excerpt">
+                    {{ substr($article->content, 0, 500) }}
+                    @if(mb_strlen($article->content) >= 500)
                     <a href="{{ route('articles.show', $article->id) }}">&hellip;</a>
-                @endif
-            </blockquote>
-        </article>
-    </li>
-    @endforeach
-</ul>
+                    @endif
+                </blockquote>
+            </article>
+        </li>
+        @endforeach
+    </ul>
+</section>
 @endsection
