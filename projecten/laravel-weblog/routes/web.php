5cdf7020983e7eb36511b653f6ca05978a9c03b4
Niriel <bdelforge@protonmail.com>
2025-02-28 11:54:20 +0100

    Fix spaces in the wrong places

--- projecten/laravel-weblog/routes/web.php
+++ projecten/laravel-weblog/routes/web.php
@@ -7,19 +7,19 @@ use App\Http\Controllers\CommentController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\AuthController;
 
-Route::get('/articles',           [ArticleController::class, 'index'    ])->name('articles.index');
-Route::get('/articles/{article}', [ArticleController::class, 'show'     ])->name('articles.show');
+Route::get('/articles',           [ArticleController::class, 'index'])->name('articles.index');
+Route::get('/articles/{article}', [ArticleController::class, 'show'])->name('articles.show');
 Route::get('/login',              [AuthController::class,    'loginPage'])->name('auth.loginPage');
-Route::post('/login',             [AuthController::class, '   login'    ])->name('auth.login');
+Route::post('/login',             [AuthController::class,    'login'])->name('auth.login');
 
-Route::group(['middleware' => ['auth']], function() {
-    Route::get('/articles/create',         [ArticleController::class, 'create' ])->name('articles.create');
-    Route::post('/articles',               [ArticleController::class, 'store'  ])->name('articles.store');
-    Route::get('/articles/{article}/edit', [ArticleController::class, 'edit'   ])->name('articles.edit');
-    Route::put('/articles/{article}',      [ArticleController::class, 'update' ])->name('articles.update');
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/articles/create',         [ArticleController::class, 'create'])->name('articles.create');
+    Route::post('/articles',               [ArticleController::class, 'store'])->name('articles.store');
+    Route::get('/articles/{article}/edit', [ArticleController::class, 'edit'])->name('articles.edit');
+    Route::put('/articles/{article}',      [ArticleController::class, 'update'])->name('articles.update');
     Route::delete('/articles/{article}',   [ArticleController::class, 'destroy'])->name('articles.destroy');
 
-    Route::post('/comments',               [CommentController::class, 'store'  ])->name('comments.store');
+    Route::post('/comments',               [CommentController::class, 'store'])->name('comments.store');
     Route::delete('/comments/{comment}',   [CommentController::class, 'destroy'])->name('comments.destroy');
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
