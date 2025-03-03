7a259f0612b394a5722186a5809d2339ef24931d
Niriel <bdelforge@protonmail.com>
2025-03-03 11:17:08 +0100

    Style delete article

--- projecten/laravel-weblog/resources/views/articles/delete.blade.php
+++ projecten/laravel-weblog/resources/views/articles/delete.blade.php
@@ -3,25 +3,53 @@
 @section('title', 'Delete article')
 
 @section('content')
-<h1>Are you certain that you want to <span class="danger">delete</span> this article?</h1>
+<header>
+    <h1>Delete article</h1>
+</header>
 
-<section id="article" class="article_box">
-    <h2 id="article_title">{{ $article->title }}</h2>
-    <div id="article_header">
-        {{ $article->created_at }}, by
-        {{ $article->user->name }}
+<section id="article_preview">
+    <p>You are about to delete the following article:</p>
+    <div class="article_preview">
+        <header class="article_header">
+            <h2 class="article_title">{{ $article->title }}</h2>
+            <div>
+                {{ $article->created_at }}, by
+                {{ $article->user->name }}
+            </div>
+        </header>
+        <main class="article_excerpt">
+            <p>
+                {{ substr($article->content, 0, 500) }}
+                @if(mb_strlen($article->content) >= 500)
+                &hellip;
+                @endif
+            </p>
+        </main>
     </div>
-    <blockquote id="article_excerpt">
-        {{ substr($article->content, 0, 500) }}&hellip;
-    </blockquote>
 </section>
 
 <section id="confirm_deletion">
-    <form action="{{ route('articles.destroy', $article->id) }}" method="POST">
-        @csrf
-        @method('DELETE')
-        <p><span class="danger">This action cannot be undone.</span></p>
-        <button type="submit" class="danger">Delete article</button>
-    </form>
+    <div class="dialog">
+        <form action="{{ route('articles.destroy', $article->id) }}" method="POST" class="real_form">
+            @csrf
+            @method('DELETE')
+            <div class="container">
+                <div class="dialog_title">Are you certain?</div>
+
+                @if ($errors->any())
+                <div>Errors:</div>
+                <ul class="errors">
+                    @foreach ($errors->all() as $error)
+                    <li class="error">{{ $error }}</li>
+                    @endforeach
+                </ul>
+                @endif
+
+                <p>You are about to delete this article.</p>
+                <p>This action cannot be undone.</p>
+                <button type="submit" class="bad">Delete article</button>
+            </div>
+        </form>
+    </div>
 </section>
 @endsection
