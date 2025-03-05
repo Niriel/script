49c08d619644ecd0a5296523eb4380c10640b02e
Niriel <bdelforge@protonmail.com>
2025-03-05 10:09:13 +0100

    Nav uses new more-generic styling

--- projecten/laravel-weblog/resources/views/partials/nav.blade.php
+++ projecten/laravel-weblog/resources/views/partials/nav.blade.php
@@ -1,15 +1,15 @@
 <nav>
-    <div class="nav_top">
-        <div class="nav_top_left">
-            <a href="{{ route('articles.index') }}" class="nav_link">Home (logo)</a>
+    <div class="nav_top left_right">
+        <div class="left">
+            <a href="{{ route('articles.index') }}" class="button_link">Home (logo)</a>
         </div>
-        <div class="nav_top_right">
+        <div class="right">
             @auth
-            <a href="{{ route('dashboard.index') }}" class="nav_link">Dashboard</a>
-            <a href="{{ route('auth.logout') }}" class="nav_link">Log out</a>
+            <a href="{{ route('dashboard.index') }}" class="button_link">Dashboard</a>
+            <a href="{{ route('auth.logout') }}" class="button_link">Log out</a>
             @endauth
             @guest
-            <a href="{{ route('auth.loginPage') }}" class="nav_link">Log in</a>
+            <a href="{{ route('auth.loginPage') }}" class="button_link">Log in</a>
             @endguest
         </div>
     </div>
