15c30e847b938647c1d3d18104d6d59c8882cbbe
Niriel <bdelforge@protonmail.com>
2025-03-10 10:20:40 +0100

    Computed properties are working

--- projecten/vue_09_yahtzee/src/components/ScoreTable.vue
+++ projecten/vue_09_yahtzee/src/components/ScoreTable.vue
@@ -10,28 +10,28 @@ const counts = computed(() => {
     for (let side = 1; side <= NB_SIDES; side++) {
         results[side] = 0;
     }
-    for (let i = 0; i < rolls.length; i++) {
-        results[rolls[i]]++;
+    for (let i = 0; i < rolls.value.length; i++) {
+        results[rolls.value[i]]++;
     }
     return results;
 });
 
-// const sumOfAllRolls = computed(() => {
-//     let sum = 0;
-//     for (const [side, count] of Object.entries(counts.value)) {
-//         console.log(count);
-//         sum += side * count;
-//     }
-//     return sum;
-// });
+const sumOfAllRolls = computed(() => {
+    let sum = 0;
+    for (const [side, count] of Object.entries(counts.value)) {
+        console.log(count);
+        sum += side * count;
+    }
+    return sum;
+});
+
 </script>
 <template>
-    <p v-for="(roll, index) in rolls" :key="index" class="die_roll">{{ roll }}</p>
     <table>
         <thead>
             <tr>
-                <th>die side</th>
-                <th>count</th>
+                <th scope="col">die side</th>
+                <th scope="col">count</th>
             </tr>
         </thead>
         <tbody>
@@ -41,6 +41,34 @@ const counts = computed(() => {
             </tr>
         </tbody>
     </table>
-    <!-- <p>{{ sumOfAllRolls }}</p> -->
+    <table>
+        <tr>
+            <th scope="row">Sum of all rolls</th>
+            <td>{{ sumOfAllRolls }}</td>
+        </tr>
+
+    </table>
 </template>
-<style scoped></style>
+
+<style scoped>
+table {
+    margin: 0.5em auto;
+    border-collapse: collapse;
+    border-top: 2px solid;
+    border-bottom: 2px solid;
+}
+td, th {
+    padding-left: 8px;
+    padding-right: 8px;
+}
+/* Table headers that form a row at the top of the table */
+th[scope="col"] {
+    text-align: center;
+    border-bottom: 1px solid;
+}
+/* Table headers that form a column on the left of the table */
+th[scope="row"] {
+    text-align: left;
+    /* border-right: 1px solid; */
+}
+</style>
