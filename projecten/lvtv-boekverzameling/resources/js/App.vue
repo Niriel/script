a9f3476bdcc72b74d3bdf59b606cb44eb38ce4dd
Niriel <bdelforge@protonmail.com>
2025-03-26 11:52:59 +0100

    More CSS

--- projecten/lvtv-boekverzameling/resources/js/App.vue
+++ projecten/lvtv-boekverzameling/resources/js/App.vue
@@ -2,19 +2,29 @@
 </script>
 
 <template>
-    <header>
+    <header class="website_header">
         <h1>Boekverzameling</h1>
+        <nav class="website_nav">
+            <menu>
+                <li><RouterLink :to="{ name: 'authors.overview' }" role="button">Authors</RouterLink></li>
+                <li><RouterLink :to="{ name: 'books.overview' }" role="button">Books</RouterLink></li>
+                <li><RouterLink :to="{ name: 'reviews.overview' }" role="button">Reviews</RouterLink></li>
+            </menu>
+        </nav>
     </header>
-    <nav class="nav_top">
-        <menu>
-            <li><RouterLink :to="{ name: 'authors.overview' }" role="button">Authors</RouterLink></li>
-            <li><RouterLink :to="{ name: 'books.overview' }" role="button">Books</RouterLink></li>
-            <li><RouterLink :to="{ name: 'reviews.overview' }" role="button">Reviews</RouterLink></li>
-        </menu>
-    </nav>
-    <main>
+    <main class="website_content">
         <RouterView />
     </main>
+    <footer class="website_footer">
+        <nav>
+            <menu>
+                <li>&copy; 2025 Script B.V.</li>
+                <li><a href="https://script.nl/privacy-cookies/">Privacy &amp; cookies</a></li>
+                <li>Help</li>
+                <li>Contact us</li>
+            </menu>
+        </nav>
+    </footer>
 </template>
 
 <style scoped></style>
